43e550e7fc747753f7912574c0ceb5b78c90ac33
ctime-zliang <1156282805@qq.com>
2022-04-07 11:29:35 +0800

    feat: modify - daily commit

--- app.js
+++ app.js
@@ -6,12 +6,15 @@ const config = require('./config/config')
 const envConfig = require('./config/env.export')
 const errorHandler = require('./lib/error')
 const logger = require('./lib/simpleLogger')
-const eventInit = require('./lib/eventInit')
+const { eventInit, eventEmitter } = require('./lib/eventInit')
+const appEventInit = require('./lib/appEventInit')
 
 const startApp = () => {
 	const app = new koa()
 
 	init(app, __dirname)
+	eventInit(app)
+	appEventInit(app)
 
 	koaEjs(app, {
 		root: path.join(__dirname, config.baseConfig.viewDir),
@@ -21,10 +24,9 @@ const startApp = () => {
 		debug: false,
 	})
 
-	eventInit(app)
-
 	app.listen(envConfig.port, envConfig.host, async () => {
 		logger.trace(`App.running - http://${envConfig.host}:${envConfig.port}`)
+		eventEmitter.emit('app/common', `App has finished starting...`)
 	})
 
 	return app
